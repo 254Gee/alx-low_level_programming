e17b3fe33c53031124fe9358f550f5430e624390
Gee <ramza10.gh@gmail.com>
2022-12-12 16:58:49 -0800

    software

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Returns: Alwasys 0
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+	int f_b;
+	int l_b;
+
+	int a2;
+	int f_b2;
+	int l_b2;
+
+	while (a <= 98)
+	{
+		f_b = (a / 10 + '0');
+		l_b = (a % 10 + '0');
+		a2 = 0;
+		while (a2 <= 99)
+		{
+			f_b2 = (a2 / 10 + '0');
+			l_b2 = (a2 % 10 + '0');
+
+			if (a < a2)
+			{
+				putchar(f_b);
+				putchar(l_b);
+				putchar(' ');
+				putchar(f_b2);
+				putchar(l_b2);
+
+				if (a != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			a2++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
