35ee918156d0b89532fcba283b53f88f3d89b364
Gee <ramza10.gh@gmail.com>
2022-12-15 17:36:30 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  * _isupper - check if a letter is uppercase
- * @c: to be checked
- * Return: 1 for upper or 0 for anything else
+ * @c: the letter to be checked
+ * Return: 1 for upper or 0 for any else
  */
 
 int _isupper(int c);
 {
+	
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
