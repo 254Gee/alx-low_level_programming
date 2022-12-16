80ca4a552b0e593e9e5f84e31671189eb5fca53d
Gee <ramza10.gh@gmail.com>
2022-12-15 19:38:11 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * Print_triangle - prints in triangle
+ * @siz: the num
+ * Return: empty
+ */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+		_putchr('\n');
+	}
+			else
+			{
+				for (x = 0; x < size; x++)
+				{
+					for (y = size - x; y > 1; y--)
+					{
+						_putchar(32);
+					}
+					for (z = 0; z <= x; z++)
+					{
+						_putchar(35);
+					}
+					_putchar('\n'
