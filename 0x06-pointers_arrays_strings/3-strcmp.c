0b17162307a3f031a6f15ddf03a8f71823bf4114
Gee <ramza10.gh@gmail.com>
2022-12-22 15:40:28 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - entry
+ * @s1: first
+ * @s2: second
+ * Return: difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		a2++
+	}
+	return (*s1 - *s2);
+}
