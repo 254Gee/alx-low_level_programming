74d360d708d432991bbd2509b657b7c2435df46a
Gee <ramza10.gh@gmail.com>
2023-06-26 03:39:42 -0700

    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,14 @@
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	for (i = 0; i < 26; i++)
 	{
-		 putchar(alp[i]);
+		putchar(alph[i]);
 	}
-	putchar('\n');
+	putchar("\n");
 	return (0);
 }
